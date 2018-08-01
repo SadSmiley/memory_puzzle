be067c06a30436a738a1fe27004fe7c6b9173ea9
Edward <edwardguevarra2003@gmail.com>
2018-08-01 16:27:42 +0800

    Generate Board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import 'bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent 
+{
+	size = {};
+	card = [];
+	submitted = false;
+
+	ngOnInit()
+	{
+		$("#size_modal").modal();
+	}
+
+	submit_size()
+	{
+		this.card = [];
+
+		for (var i = 0; i < this.size.height; i++) 
+		{
+			this.card[i] = [];
+
+			for (var x = 0; x < this.size.width; x++) 
+			{
+				this.card[i][x] = null;
+			}
+		}
+
+		this.submitted = true;
+	}
 }
